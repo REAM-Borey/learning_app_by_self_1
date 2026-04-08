4e0be8f1ff2482faaec3dae0217b8634e7cd8eef
REAM-Borey <boreyreun168@gmail.com>
2026-04-08 23:40:12 +0700

    borey commit on 8-4

--- lib/presentation/screens/homepage.dart
+++ lib/presentation/screens/homepage.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learning_flutter_1/presentation/bloc/counter_bloc.dart';
 import 'package:learning_flutter_1/presentation/bloc/counter_event.dart';
 import 'package:learning_flutter_1/presentation/bloc/counter_state.dart';
 import 'package:learning_flutter_1/presentation/screens/secondpage.dart';
+import 'package:learning_flutter_1/services/product_service.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,6 +17,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _productService = ProductService();
+
+  @override
+  void initState() {
+    _getData();
+    super.initState();
+  }
+
+  Future<void> _getData() async {
+    await _productService.fetchAllProducts();
+    setState(() {
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
